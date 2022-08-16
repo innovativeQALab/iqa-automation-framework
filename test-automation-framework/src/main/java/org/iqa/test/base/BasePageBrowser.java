e3be9572d07a708168137022b047aa430b1eb21b
Prashant <Prashant@DESKTOP-ALPRI2S>
2022-08-16 22:38:21 +0530

    Added selenium reusable methods

--- test-automation-framework/src/main/java/org/iqa/test/base/BasePageBrowser.java
+++ test-automation-framework/src/main/java/org/iqa/test/base/BasePageBrowser.java
@@ -8,11 +8,17 @@ import org.iqa.suite.commons.ExtentLogger;
 import org.iqa.suite.commons.applitool.ApplitoolEyesWeb;
 import org.iqa.test.test_data.RuntimeTestDataHolder;
 import org.iqa.test.webdriver_factory.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.asserts.SoftAssert;
 import com.applitools.eyes.selenium.Eyes;
 
@@ -21,9 +27,11 @@ public class BasePageBrowser {
 	protected SoftAssert softAssert;
 	protected Wait<WebDriver> fluentwait;
 	protected WebDriver driver;
-	public ExtentLogger logger = new ExtentLogger();
+	public ExtentLogger extentLogger = new ExtentLogger();
+	private static final Logger logger = LoggerFactory.getLogger(BasePageBrowser.class);
 	public Eyes eye = ApplitoolEyesWeb.getEyesForPageObject();
 	private Map<String, String> runTimeTestData = new HashMap<String, String>();
+	private static final int DEFAULT_WAIT_TIMEOUT = 30;
 
 	public BasePageBrowser() {
 		driver = WebDriverFactory.getDriver();
@@ -53,6 +61,132 @@ public class BasePageBrowser {
 					+ ". Confirm if it is intended.");
 		}
 		runTimeTestData.put(key, value);
+	}
+
+	/**
+	 * Navigate to given URL
+	 *
+	 * @param String url
+	 */
+	protected void navigateToUrl(String url) {
+		driver.get(url);
+		driver.manage().window().maximize();
+	}
+
+	/**
+	 * Wait for an element to be visible on the screen, given a maximum time to wait
+	 * 
+	 * @param WebElement element
+	 * @param int        timeOutInSeconds
+	 * @return WebElement
+	 */
+	protected WebElement waitForElementToBeVisible(WebElement element, int timeOutInSeconds) {
+		WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+
+		try {
+			return wait.until(ExpectedConditions.visibilityOf(element));
+		} catch (Exception e) {
+			logger.error("!!!!!!!ERROR Element is not visible\n" + e.getMessage());
+			return null;
+		}
+	}
+
+	/**
+	 * Wait for an element to be visible on the screen, using the
+	 * DEFAULT_WAIT_TIMEOUT
+	 * 
+	 * @param WebElement element
+	 * @return WebElement
+	 */
+	protected WebElement waitForElementToBeVisible(WebElement element) {
+		return waitForElementToBeVisible(element, DEFAULT_WAIT_TIMEOUT);
+	}
+
+	/**
+	 * Wait for an element to be clickable, given a maximum time to wait
+	 * 
+	 * @param WebElement element
+	 * @param int        timeOutInSeconds
+	 * @return WebElement
+	 */
+	protected WebElement waitForElementToBeClickable(WebElement element, int timeOutInSeconds) {
+		WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+
+		try {
+			return wait.until(ExpectedConditions.elementToBeClickable(element));
+		} catch (Exception e) {
+			logger.error("!!!!!!!ERROR Element is not clickable\n" + e.getMessage());
+			return null;
+		}
+	}
+
+	/**
+	 * Wait for an element to be visible on the screen, using the
+	 * DEFAULT_WAIT_TIMEOUT
+	 * 
+	 * @param WebElement element
+	 * @return WebElement
+	 */
+	protected WebElement waitForElementToBeClickable(WebElement element) {
+		return waitForElementToBeClickable(element, DEFAULT_WAIT_TIMEOUT);
+	}
+
+	/**
+	 * Click on WebElement
+	 * 
+	 * @param WebElement element
+	 */
+	protected void clickOnElement(WebElement element) {
+		waitForElementToBeClickable(element, DEFAULT_WAIT_TIMEOUT);
+		try {
+			element.click();
+		} catch (Exception e) {
+			logger.error("!!!!!!!ERROR Element is not clickable\n" + e.getMessage());
+		}
+	}
+
+	/**
+	 * Clear Text field
+	 * 
+	 * @param WebElement element
+	 */
+	protected void enterText(WebElement element) {
+		waitForElementToBeVisible(element, DEFAULT_WAIT_TIMEOUT);
+		try {
+			element.clear();
+		} catch (Exception e) {
+			logger.error("!!!!!!!ERROR in clearing the element\n" + e.getMessage());
+		}
+	}
+
+	/**
+	 * Enter text in field
+	 * 
+	 * @param WebElement element
+	 * @Param String textToBeEntered
+	 */
+	protected void enterText(WebElement element, String textToBeEntered) {
+		waitForElementToBeVisible(element, DEFAULT_WAIT_TIMEOUT);
+		try {
+			element.sendKeys(textToBeEntered);
+		} catch (Exception e) {
+			logger.error("!!!!!!!ERROR in sending text to the element\n" + e.getMessage());
+		}
+	}
+
+	/**
+	 * Click on WebElement using JavaScriptExecutor
+	 * 
+	 * @param WebElement element
+	 */
+	protected void clickOnElementUsingJS(WebElement element) {
+
+		JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+		try {
+			jsExecutor.executeScript("arguments[0].click();", element);
+		} catch (Exception e) {
+			logger.error("!!!!!!!ERROR Element is not clickable using JavascriptExecutor\n" + e.getMessage());
+		}
 	}
 
 }
