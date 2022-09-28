757f10702684d9919804bc44dd93dc79b4755a18
Prashant <pbhange@spendlabs.com>
2022-09-28 12:39:57 +0530

    Refactoring of BasePageMobile & Added Wait util methods

--- test-automation-framework/src/main/java/org/iqa/test/base/BasePageMobile.java
+++ test-automation-framework/src/main/java/org/iqa/test/base/BasePageMobile.java
@@ -1,59 +1,94 @@
 package org.iqa.test.base;
 
 import java.time.Duration;
-
-import org.iqa.suite.commons.AssertionFactory;
+import java.util.HashMap;
+import java.util.Map;
 import org.iqa.suite.commons.ExtentLogger;
-import org.iqa.suite.commons.PropertyHolder;
-import org.iqa.suite.commons.TestMetaData;
 import org.iqa.suite.commons.applitool.ApplitoolEyesWeb;
-import org.iqa.suite.commons.applitool.ApplitoolEyesMobile;
+import org.iqa.test.test_data.RuntimeTestDataHolder;
 import org.iqa.test.webdriver_factory.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.TestException;
 import org.testng.asserts.SoftAssert;
-
+import com.applitools.eyes.selenium.Eyes;
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import io.cucumber.testng.FeatureWrapper;
-import io.cucumber.testng.PickleWrapper;
 
 public class BasePageMobile {
-	protected final static int DEFAULTWAITSECONDS = 10;
-	protected final static int MINWAITSECONDS = 1;
-	protected final static int MAXWAITSECONDS = 15;
+
 	protected SoftAssert softAssert;
 	@SuppressWarnings("rawtypes")
 	protected Wait<AppiumDriver> fluentwait;
-	protected AppiumDriver<MobileElement> driver;
-	public ExtentLogger logger = new ExtentLogger();
-	@SuppressWarnings("unchecked")
-	public BasePageMobile()
-	{
-		System.out.println("******* In Base Page mobile");
-		driver = (AppiumDriver<MobileElement>) WebDriverFactory.getDriver();
-		//driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
-		fluentwait = getFluentWaitObject(15);
-		softAssert = new SoftAssert();
-		AssertionFactory.setSoftAssert(softAssert);
-		
-		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
-		
+	@SuppressWarnings("rawtypes")
+	protected AppiumDriver driver;
+	protected ExtentLogger extentLogger = new ExtentLogger();
+	protected static final Logger logger = LoggerFactory.getLogger(BasePageBrowser.class);
+	private static final int DEFAULT_WAIT_TIMEOUT = 30;
+	protected Eyes eye = ApplitoolEyesWeb.getEyesForPageObject();
+	private Map<String, String> runTimeTestData = new HashMap<String, String>();
+
+	@SuppressWarnings("rawtypes")
+	public BasePageMobile() {
+		try {
+			logger.info("*******In BasePageMobile");
+			driver = (AppiumDriver) WebDriverFactory.getDriver();
+			fluentwait = getFluentWaitTimeout();
+			PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+			runTimeTestData = RuntimeTestDataHolder.getRunTimeTestData();
+		} catch (Exception e) {
+			logger.error(
+					"!!!!!!!!!ERROR AppiumDriver driver found as NULL. Please check if AUT property is correctly set as AUT=MOBILE in Environment.properties file. Exiting...");
+			System.exit(-1);
+		}
 
 	}
+
+	/**
+	 * Get FluentWait with DEFAULT_WAIT_TIMEOUT
+	 * 
+	 * @return Wait<WebDriver>
+	 */
+
 	@SuppressWarnings("rawtypes")
-	public Wait<AppiumDriver> getFluentWaitObject(int timeout)
-	{
-		Wait<AppiumDriver> wait = new FluentWait<AppiumDriver>((AppiumDriver) driver)        
-				.withTimeout(Duration.ofSeconds(10) )        
-				.pollingEvery(Duration.ofSeconds(1))        
-				.ignoring(NoSuchElementException.class);
-		return wait;
+	protected Wait<AppiumDriver> getFluentWaitTimeout() {
+		return new FluentWait<AppiumDriver>((AppiumDriver) driver) .withTimeout(Duration.ofSeconds(DEFAULT_WAIT_TIMEOUT))
+				.pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+	}
+
+	/**
+	 * Get FluentWait with given TIMEOUT
+	 * 
+	 * @param int timeOutInSeconds
+	 * @return Wait<WebDriver>
+	 */
+
+	protected Wait<WebDriver> getFluentWaitTimeout(int timeOutInSeconds) {
+		return new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(timeOutInSeconds))
+				.pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+	}
+
+	public String getRunTimeTestData(String key) {
+		return runTimeTestData.get(key);
+	}
+
+	public void setRunTimeTestData(String key, String value) {
+
+		if (runTimeTestData.containsKey(key)) {
+			logger.info("!!!!!!!!!! Duplicate Runtime Data Key inserstion found. Key " + key
+					+ " was already present in Map with value " + runTimeTestData.get(key)
+					+ ". Same key is being asked to insert again, so, overwriting it with new value " + value
+					+ ". Confirm if it is intended.");
+		}
+		runTimeTestData.put(key, value);
 	}
 	
-	 
 
 }
