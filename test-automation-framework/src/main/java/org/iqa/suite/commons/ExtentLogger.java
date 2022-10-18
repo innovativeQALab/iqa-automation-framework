4f91347bde505f4190a2bd3c3d7d322e97e6ff42
Vaibhav Zodge <zodgevaibhav@gmail.com>
2022-10-18 14:17:52 +0530

    Keep supporting ExtenrTestReportFactory

--- test-automation-framework/src/main/java/org/iqa/suite/commons/ExtentLogger.java
+++ test-automation-framework/src/main/java/org/iqa/suite/commons/ExtentLogger.java
@@ -2,28 +2,42 @@ package org.iqa.suite.commons;
 
 import org.iqa.suite.commons.reporting.ExtentReportTestFactory;
 
+import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
 
 public class ExtentLogger {
 
-	public void info(String message) {
+	public ExtentLogger info(String message) {
 		ExtentReportTestFactory.getTest().log(Status.INFO, message);
+		return this;
 	}
-	
-	public void error(String message) {
-		//ExtentReportTestFactory.getTest().error(message);
-	}
-	
-	public void error(Throwable thorowable) {
-		//ExtentReportTestFactory.getTest().error(thorowable);
-	}
-	
-	public void fail(String message) {
+
+	@Deprecated
+	public ExtentLogger error(String message) {
 		ExtentReportTestFactory.getTest().fail(message);
+		return this;
 	}
-	
-	public void fail(Throwable thorowable) {
+
+	@Deprecated
+	public ExtentLogger error(Throwable thorowable) {
 		ExtentReportTestFactory.getTest().fail(thorowable);
+		return this;
+	}
+
+	public ExtentLogger fail(String message) {
+		ExtentReportTestFactory.getTest().fail(message);
+		return this;
+	}
+
+	public ExtentLogger fail(Throwable thorowable) {
+		ExtentReportTestFactory.getTest().fail(thorowable);
+		return this;
+	}
+
+	public ExtentLogger attachScreenshot() {
+		ExtentReportTestFactory.getTest().log(Status.INFO,
+				MediaEntityBuilder.createScreenCaptureFromBase64String(SeleniumUtils.getScreenshotAsBase64()).build());
+		return this;
 	}
 
 }
