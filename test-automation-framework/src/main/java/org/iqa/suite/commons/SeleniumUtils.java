22294e8be8f8d48bf2bdfc9700210df427609fcf
Prashant <prashantbbhange@gmail.com>
2022-11-25 13:05:41 +0530

    Add getOsFamilyName method to derive OS family name from platform

--- test-automation-framework/src/main/java/org/iqa/suite/commons/SeleniumUtils.java
+++ test-automation-framework/src/main/java/org/iqa/suite/commons/SeleniumUtils.java
@@ -2,7 +2,6 @@ package org.iqa.suite.commons;
 
 import java.io.File;
 import java.io.IOException;
-
 import org.apache.commons.io.FileUtils;
 import org.iqa.test.webdriver_factory.WebDriverFactory;
 import org.openqa.selenium.OutputType;
@@ -11,14 +10,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SeleniumUtils {
-    private static final Logger logger = LoggerFactory.getLogger(SeleniumUtils.class);
+	private static final Logger logger = LoggerFactory.getLogger(SeleniumUtils.class);
 
 	public static void takeScreenshot(String fileNameWithPath) {
 		if (null == WebDriverFactory.getDriver()) {
 			logger.info("!!!!!! Webdriver is null hence returning from SeleniumUtil > takeScreenShot()");
 			return;
 		}
-		logger.info("********** Taking screenshot at - "+fileNameWithPath);
+		logger.info("********** Taking screenshot at - " + fileNameWithPath);
 		File scrFile = ((TakesScreenshot) WebDriverFactory.getDriver()).getScreenshotAs(OutputType.FILE);
 		try {
 			FileUtils.copyFile(scrFile, new File(fileNameWithPath));
@@ -27,14 +26,39 @@ public class SeleniumUtils {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static String getScreenshotAsBase64() {
 		if (null == WebDriverFactory.getDriver()) {
 			logger.info("!!!!!! Webdriver is null hence returning from SeleniumUtil > takeScreenShot()");
 			return null;
 		}
 		logger.info("********** Taking screenshot as Base64 ");
-		return  ((TakesScreenshot) WebDriverFactory.getDriver()).getScreenshotAs(OutputType.BASE64);	
+		return ((TakesScreenshot) WebDriverFactory.getDriver()).getScreenshotAs(OutputType.BASE64);
 	}
+
+	/**
+	 * Returns OS Family name based on platform name
+	 *
+	 * @param platformProperty/*
+	 * @return String osFamilyName
+	 */
+	public static String getOsFamilyName(String platformProperty) {
+		String platform = platformProperty.toLowerCase();
+		if (platform.contains("windows") || platform.contains("win")) {
+			return "WINDOWS";
+		} else if (platform.contains("linux")) {
+			return "LINUX";
+		} else if (platform.contains("mac")) {
+			return "MAC";
+		} else if (platform.contains("ios")) {
+			return "IOS";
+		} else if (platform.contains("android")) {
+			return "ANDROID";
+		} else {
+			throw new IllegalArgumentException(
+					"!!! Platform property " + platformProperty + " is not currently supported with this version of framework!!!");
+		}
+	}
+	
 
 }
